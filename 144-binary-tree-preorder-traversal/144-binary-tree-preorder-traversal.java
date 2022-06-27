21e4de030b253ff255e9ad960192a97c7bcb9033
JyotsnaBN <70506684+JyotsnaBN@users.noreply.github.com>
2022-06-27 10:37:12 +0530

    Time: 2 ms (48.65%), Space: 42.5 MB (30.14%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -18,17 +18,23 @@ class Solution
     public List<Integer> preorderTraversal(TreeNode root) 
     {
         List<Integer> list = new ArrayList<Integer> ();
-        func(root, list);
+        Stack<TreeNode> stack = new Stack<TreeNode> ();
+        if(root == null)
+            return list;
+        stack.push(root);
+        TreeNode curr = root;
+        while(!stack.isEmpty())
+        {
+            curr = stack.pop();
+            list.add(curr.val);
+            if(curr.right!=null)
+                stack.push(curr.right);
+            if(curr.left!=null)
+                stack.push(curr.left);
+                
+        }
         return list;
         
     }
-    public void func(TreeNode root, List<Integer> list )
-    {
-        if(root==null)
-            return;
-        list.add(root.val);
-        func(root.left, list);
-        func(root.right, list);
-        
-    }
+
 }
