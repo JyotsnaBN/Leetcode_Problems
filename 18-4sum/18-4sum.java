03c98a7f8234f102d9a0b9271a00ecf523c16a9d
JyotsnaBN <70506684+JyotsnaBN@users.noreply.github.com>
2022-07-13 11:20:34 +0530

    Time: 828 ms (5.00%), Space: 118.4 MB (5.14%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -4,28 +4,18 @@ class Solution
     {
         Set<List<Integer>> res = new HashSet<List<Integer>> ();
         int n = nums.length;
-        
         Arrays.sort(nums);
-        
-        
-        
         for(int i = 0; i<n; i++)
         {
             for(int j = i+1; j<n; j++)
             {
                 for(int k = j+1; k<n; k++)
                 {
-                    int l = k+1, r = n-1;
-                    while(l<=r)
+                    for(int l = k+1; l<n; l++)
                     {
-                        long temp1 = (long)nums[i]+nums[j]+nums[k]+nums[l], temp2 = (long)nums[i]+nums[j]+nums[k]+nums[r];
-                        if(temp1<target)
-                        {
-                            l++;
-                        }
-                        else if(temp1==target)
-                        {
-                            List<Integer> list = new ArrayList<Integer> ();
+                        if((long)nums[i]+nums[j]+nums[k]+nums[l] != target)
+                            continue;
+                        List<Integer> list = new ArrayList<Integer> ();
                         list.add(nums[i]);
                         list.add(nums[j]);
                         list.add(nums[k]);
@@ -33,25 +23,7 @@ class Solution
                         Collections.sort(list);
                         
                         res.add(list);
-                            break;
-                        }
-                        if(temp2>target)
-                        {
-                            r--;
-                        }
-                        else if(temp2==target)
-                        {
-                            List<Integer> list = new ArrayList<Integer> ();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        list.add(nums[r]);
-                        Collections.sort(list);
-                        
-                        res.add(list);
-                            break;
-                        }
-                        
+                        break;
                     }
                 }
             }
