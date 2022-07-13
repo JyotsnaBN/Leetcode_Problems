038318d656b1ff3b1b1a8089c427d1c8eb98a11d
JyotsnaBN <70506684+JyotsnaBN@users.noreply.github.com>
2022-07-13 11:40:16 +0530

    Time: 398 ms (8.02%), Space: 118 MB (7.51%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,3 +1,62 @@
+//O(n^3) solution
+class Solution 
+{
+    public List<List<Integer>> fourSum(int[] nums, int target) 
+    {
+        Set<List<Integer>> res = new HashSet<List<Integer>> ();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i = 0; i<n; i++)
+        {
+            for(int j = i+1; j<n; j++)
+            {
+                int l = j+1, r = n-1;
+                while(l<r)
+                {
+                    long temp = (long)nums[i]+nums[j]+nums[l]+nums[r];
+                    //System.out.println(nums[i] + " " + nums[j] + " " +nums[l] + " " +nums[r] + " " + temp);
+                    if(temp<target)
+                        l++;
+                    else if(temp>target)
+                        r--;
+                    else
+                    {
+                        List<Integer> list = new ArrayList<Integer> ();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[l]);
+                        list.add(nums[r]);
+                        Collections.sort(list);
+                        
+                        res.add(list);
+                        
+                        l++;
+                    }
+                }     
+                
+                
+            }
+            //System.out.println();
+        }
+        List<List<Integer>> result = new ArrayList<List<Integer>> ();
+        for(List<Integer> i : res)
+            result.add(i);
+        
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
+//O(n^4) solution
+
 class Solution 
 {
     public List<List<Integer>> fourSum(int[] nums, int target) 
@@ -35,3 +94,19 @@ class Solution
         return result;
     }
 }
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
