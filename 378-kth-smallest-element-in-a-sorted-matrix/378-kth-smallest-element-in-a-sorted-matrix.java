888e2cda7be3c4c5c7b2cc2bea58dc36db37e24c
JyotsnaBN <70506684+JyotsnaBN@users.noreply.github.com>
2022-08-02 09:57:56 +0530

    Time: 100 ms (5.05%), Space: 57 MB (44.57%) - LeetHub

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -0,0 +1,72 @@
+class Solution 
+{
+    public int kthSmallest(int[][] mat, int k) 
+    {
+        PriorityQueue<Element> q = new PriorityQueue<Element>(new ElementComparator());
+        Set<String> vis = new HashSet<String> ();
+        int n = mat.length;
+        
+        vis.add("" + 0 + "," + 0);
+        q.add(new Element(0, 0, mat[0][0]));
+        
+        
+        int c = 0;
+        
+        while(!q.isEmpty())
+        {
+            int i = q.peek().i;
+            int j = q.peek().j;
+            int val = q.poll().val;
+            c++;
+            if(c==k)
+                return val;
+            
+            
+            if(i+1<n)
+            {
+                String t = "" + (i + 1) + "," + j;
+                if(!vis.contains(t))
+                {
+                    vis.add(t);
+                    q.add(new Element(i+1, j, mat[i+1][j]));
+                }
+            }
+            if(j+1<n)
+            {
+                String t = "" + i + "," + (j + 1);
+                if(!vis.contains(t))
+                {
+                    vis.add(t);
+                    q.add(new Element(i, j + 1, mat[i][j + 1]));
+                }
+            }
+            
+        }
+        
+        
+        return 0;
+    }
+}
+
+
+class Element
+{
+    int i, j, val;
+    Element(int i, int j, int val)
+    {
+        this.i = i;
+        this.j = j;
+        this.val = val;
+    }
+}
+class ElementComparator implements Comparator<Element>
+{
+    public int compare(Element e1, Element e2)
+    {
+        if(e1.val<e2.val)
+            return -1;
+        else if(e1.val>e2.val)
+            return 1;
+        return 0;
+    }
+}
