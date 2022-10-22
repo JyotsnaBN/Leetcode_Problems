adb550e96ae8e622dbd2b7ad4c18384566bf7356
JyotsnaBN <70506684+JyotsnaBN@users.noreply.github.com>
2022-10-22 21:58:46 +0530

    Time: 15 ms (73.82%), Space: 50.9 MB (26.64%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -4,11 +4,9 @@ class Solution
     {
         String res = "";
         int m = s.length(), n = t.length();
+        
         if(m<n)
             return res;
-        //System.out.println(m + " " + n);
-
-        
         
         int map1[] = new int[128];
         int map2[] = new int[128];
@@ -16,22 +14,23 @@ class Solution
         int have = 0, need = 0;
         s += "0";
         m++;
-        for(int i = 0; i<n; i++)
+        
+        for(int i = 0; i<n; i++)//Add all characters in t to map2 with its count
         {
             map2[(int)t.charAt(i)]++;
-            if(map2[(int)t.charAt(i)] == 1)
+            if(map2[(int)t.charAt(i)] == 1)//count the number of unique chars in this string
                 need++;
         }
         
         int i = 0, j = 0;
-        for(j = 0; j<n; j++)
+        for(j = 0; j<n; j++)//Add the 1st n characters of s to map1 with its count
         {
             char c = s.charAt(j);
             
             map1[(int)c]++;
             
             
-            if(map2[(int)c] == map1[(int)c] && map2[(int)c] != 0)
+            if(map2[(int)c] == map1[(int)c] && map2[(int)c] != 0)//check how many of the unique chars from t are present in s
                 have++;
         }
         
@@ -41,9 +40,17 @@ class Solution
         
         boolean flag = true;
         
+        /*
+        Use a sliding window approach
+        i to track the beginning of the substring and j the end
+        Add chars from the right till we have all the chars from t in that substring of s                                      ---   j++
+        Then remove chars from the left while having all chars from t in the substring of s                                    ---   i--
+        When we reach a point where we cannot remove any more chars from the left, start adding chars from right again         ---   j++
+        This switch is achieved using flag
+        */
+        
         while(i<m-n && j<m)
-        {
-            
+        {      
             if(flag)
             {
                 char c = s.charAt(j);
@@ -67,7 +74,6 @@ class Solution
             if(!flag)
             {
                 char c = s.charAt(i);
-                //map1.put(c, map1.get(c) - 1);
                 map1[(int)c]--;
                 
                 if(map2[(int)c] <= map1[(int)c] )
