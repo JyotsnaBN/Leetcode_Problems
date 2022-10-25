60963131120b623425caf6bff0a658d4e3f4d818
JyotsnaBN <70506684+JyotsnaBN@users.noreply.github.com>
2022-10-25 10:24:14 +0530

    Time: 67 ms (89.29%), Space: 86.5 MB (76.10%) - LeetHub

--- 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.java
+++ 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.java
@@ -3,74 +3,50 @@ class Solution
     public long makeSimilar(int[] nums, int[] target) 
     {
         int n = nums.length;
+        int i = 0, j = 0;
         if(n == 1)
             return 0;
-        if(n == 2)
-        {
-            
-        }
+
         Arrays.sort(nums);
         Arrays.sort(target);
-        List<Integer> e = new ArrayList<Integer>();
-        List<Integer> o = new ArrayList<Integer>();
-        for(int i : nums)
-        {
-            if(i%2==0)
-                e.add(i);
-            else
-                o.add(i);
-        }
+        
+        
         int arr[] = new int[n];
         
-        int i = 0, j = 0;
-        for(i = 0; i<e.size(); i++)
+        for(int k : nums)
         {
-            arr[i] = e.get(i);
+            if(k%2==0)
+                arr[i++] = k;
         }
-        for(j = 0; j<o.size(); j++)
+        for(int k : nums)
         {
-            arr[i++] = o.get(j);
+            if(k%2!=0)
+                arr[i++] = k;
         }
         
-        e = new ArrayList<Integer>();
-        o = new ArrayList<Integer>();
+        i = 0;
+        long sum = 0;
         for(int k : target)
         {
             if(k%2==0)
-                e.add(k);
-            else
-                o.add(k);
+            {    
+                arr[i] = k - arr[i];
+                if(arr[i]>0)
+                    sum += arr[i];
+                i++;
+            }
         }
-        long sum = 0;
-        for(i = 0; i<e.size(); i++)
+        for(int k : target)
         {
-            arr[i] = e.get(i) - arr[i];
-            
-            if(arr[i]>0)
-                sum+=arr[i];
+            if(k%2!=0)
+            {    
+                arr[i] = k - arr[i];
+                if(arr[i]>0)
+                    sum += arr[i];
+                i++;
+            }
         }
-        for(j = 0; j<o.size(); j++)
-        {
-            arr[i] = o.get(j) - arr[i];
-            if(arr[i]>0)
-                sum+=arr[i];
-            i++;
-        }
-        
-        //System.out.println(arr);
-        //for(i = 0; i<n; i++)
-        //{
-           // System.out.print(arr[i] + "  ");
-        //}
-        //System.out.println();
-        
-        
-        
-        
-        
-        
-        
-        
+
         
         return sum/2;
         
