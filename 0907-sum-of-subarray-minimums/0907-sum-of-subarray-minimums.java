56c101ecfb6650c0c6889545986196dd24993ddc
JyotsnaBN <70506684+JyotsnaBN@users.noreply.github.com>
2022-11-25 21:36:45 +0530

    Time: 185 ms (17.43%), Space: 76.4 MB (5.50%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,44 +1,33 @@
 class Solution 
 {
-    int n;
     public int sumSubarrayMins(int[] arr) 
     {
-        n = arr.length;
+        int n = arr.length;
         long M = 1000000007;
         int res = 0;
-        Stack<Pair<Integer, Integer>> s = new Stack<>();
         
+        Stack<Pair<Integer, Integer>> s = new Stack<>();      
         s.push(new Pair(0,-1));
-        
         
         int temp1[] = new int[n];
         int temp2[] = new int[n];
-
-        
-        int ind = 0;
         
         for(int i = 0; i<n; i++)
         {
-   
-                while(s.peek().getKey()>=arr[i])
-                    s.pop();
+            while(s.peek().getKey()>=arr[i])
+                s.pop();
 
-            
-            
             temp1[i] += (i - s.peek().getValue());
             s.push(new Pair(arr[i],i));
         }
         
-        
         s.clear();
         s.push(new Pair(0, n));
         
-        
         for(int i = n-1; i>=0; i--)
         {
-    
-                while(s.peek().getKey()>arr[i])
-                    s.pop();  
+            while(s.peek().getKey()>arr[i])
+                s.pop();  
                
             temp2[i] += (s.peek().getValue() - i);
             s.push(new Pair(arr[i],i));
@@ -51,7 +40,6 @@ class Solution
             res %= M;
         }
         
-
         return res;
             
     }
